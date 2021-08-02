a364fcec33eac7fc53cba6d7f9a29d0e5cfe484b
Nirav Mendapara <nirav3998@gmail.com>
2021-08-02 17:53:45 +0530

    hii

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var session = require('express-session')
 var path = require('path');
 //var favicon = require('serve-favicon');
 //var logger = require('morgan');
@@ -14,7 +15,7 @@ var app = express();
 // view engine setup
 app.use(express.static(__dirname + '../public'));
 app.set('trust proxy',1);
-app.use(Session({
+app.use(session({
   secret : 'anaplan',
   resave : false,
   saveUninitialized : true,
