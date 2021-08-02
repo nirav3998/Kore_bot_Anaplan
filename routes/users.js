1390390d86808422efc0db4711bb28fc9890f9ae
Nirav Mendapara <nirav3998@gmail.com>
2021-08-02 10:40:36 +0530

    hii

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var config = require('../config');
+
+var jwt = require('jsonwebtoken');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resources');
+});
+
+
+
+router.post('/', function(req, res, next) {
+  var identity = req.body.identity;
+  var clientId = req.body.clientId;
+  var clientSecret = req.body.clientSecret;
+  var isAnonymous = req.body.isAnonymous || false;
+  var aud = req.body.aud || "https://idproxy.kore.com/authorize";
+  //var aud = "https://idproxy.kore.com/authorize";
+  //res.send("i have written");
+  console.log("hiiii");
+  var options = {
+    "jti":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWlsdG86bWlrZUBmb28uY29tIn0.Cd7gS9Vk4BiTjNPxhN6R14wAnR70CMW5v-AzfUrsNHU",
+    "iat": new Date().getTime(),
+    "exp": (new Date(new Date().getTime() + 60 * 59 * 1000).getTime())/1000,
+    "aud": aud,
+    "iss": clientId,
+    "sub": identity,
+    "isAnonymous": isAnonymous
+  }
+	var token = jwt.sign(options, clientSecret);
+  res.header('Access-Control-Allow-Origin', "*");
+  res.send({"jwt":token});
 });
 
 module.exports = router;
---
+++ sdk/UI/kore-config.js
@@ -0,0 +1,64 @@
+(function(KoreSDK){
+
+    var KoreSDK=KoreSDK||{};
+
+    var botOptions = {};
+    botOptions.logLevel = 'debug';
+    botOptions.koreAPIUrl = "https://bots.kore.ai/api/";
+    botOptions.koreSpeechAPIUrl = "";//deprecated
+    //botOptions.bearer = "bearer xyz-------------------";
+    //botOptions.ttsSocketUrl = '';//deprecated
+    botOptions.koreAnonymousFn = koreAnonymousFn;
+    botOptions.recorderWorkerPath = '../libs/recorderWorker.js';
+
+    botOptions.JWTUrl = "http://127.0.0.1:3000/api/users/sts";
+    botOptions.userIdentity = 'mendapara.nirav@tcs.com';// Provide users email id here
+    botOptions.botInfo = { name: "Anaplan_Assistant_2", "_id": "st-05c4e6b7-6e85-5fdf-bfed-b66ce480b71b" }; // bot name is case sensitive
+    botOptions.clientId = "cs-812e4377-3a2a-5304-a83d-1095b50f6ff0";
+    botOptions.clientSecret = "B8g8Sw0dm+H18qS+bE5cz8np7HpxjDd6GSvlnHUsl0w=";
+	
+// To modify the web socket url use the following option
+// botOptions.reWriteSocketURL = {
+//     protocol: 'PROTOCOL_TO_BE_REWRITTEN',
+//     hostname: 'HOSTNAME_TO_BE_REWRITTEN',
+//     port: 'PORT_TO_BE_REWRITTEN'
+// };
+    
+    var chatConfig={
+        isAnonymous:true,
+        botOptions:botOptions,
+        allowIframe: false, 			// set true, opens authentication links in popup window, default value is "false"
+        isSendButton: false, 			// set true, to show send button below the compose bar
+        isTTSEnabled: false,			// set true, to hide speaker icon
+        ttsInterface:'webapi',        // webapi or awspolly , where default is webapi
+        isSpeechEnabled: false,			// set true, to hide mic icon
+        allowGoogleSpeech: true,		// set true, to use Google speech engine instead KORE.AI engine.This feature requires valid Google speech API key. (Place it in 'web-kore-sdk/libs/speech/key.js')
+        allowLocation: false,			// set false, to deny sending location to server
+        loadHistory: false,				// set true to load recent chat history
+        messageHistoryLimit: 0,		// set limit to load recent chat history
+        autoEnableSpeechAndTTS: false, 	// set true, to use talkType voice keyboard.
+        graphLib: "d3" ,				// set google, to render google charts.This feature requires loader.js file which is available in google charts documentation.
+        googleMapsAPIKey:"",
+        minimizeMode: false,             // set true, to show chatwindow in minimized mode, If false is set remove #chatContainer style in chatwindow.css  
+        multiPageApp: {
+            enable: false,              //set true for non SPA(Single page applications)
+            userIdentityStore: 'localStorage',//'localStorage || sessionStorage'
+            chatWindowStateStore: 'localStorage'//'localStorage || sessionStorage'
+        },              
+        supportDelayedMessages:true,    // enable to add support for renderDelay in message nodes which will help to render messages with delay from UI       
+        pickersConfig:{
+            showDatePickerIcon:false,           //set true to show datePicker icon
+            showDateRangePickerIcon:false,      //set true to show dateRangePicker icon
+            showClockPickerIcon:false,          //set true to show clockPicker icon
+            showTaskMenuPickerIcon:false,       //set true to show TaskMenu Template icon
+            showradioOptionMenuPickerIcon:false //set true to show Radio Option Template icon
+        }
+    };
+     /* 
+        allowGoogleSpeech will use Google cloud service api.
+        Google speech key is required for all browsers except chrome.
+        On Windows 10, Microsoft Edge will support speech recognization.
+     */
+
+    KoreSDK.chatConfig=chatConfig
+})(window.KoreSDK);
